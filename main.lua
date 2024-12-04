daa5c222d286d3a1c93560b74873a3e06610fce6
EmptySyntax <156080355+EmptySyntax@users.noreply.github.com>
2024-12-04 12:03:21 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,3 @@
+if game.Players.LocalPlayer.UserId == 16732694052 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EmptySyntax/SyntaxCore/refs/heads/main/Games/fisch.lua"))()
+end
