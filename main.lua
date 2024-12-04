c9535a9873ff1d2e4d59be1bf25853734f39360f
EmptySyntax <156080355+EmptySyntax@users.noreply.github.com>
2024-12-04 12:04:37 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
-if game.Players.LocalPlayer.UserId == 16732694052 then
+if game.PlaceId == 16732694052 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EmptySyntax/SyntaxCore/refs/heads/main/Games/fisch.lua"))()
 end
